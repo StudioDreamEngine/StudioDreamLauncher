55f848b3d8f2eb05f820d98c5422254c07d4ddd4
sonickirb <sonicyan11@gmail.com>
2026-07-04 19:32:07 -0500

    visuals

--- src/conf.lua
+++ src/conf.lua
@@ -2,6 +2,7 @@ function love.conf(t)
     t.window.width      = 1920/2
     t.window.height     = 1080/2
     t.window.title      = "StudioDream Launcher"
+    t.window.icon       = "Assets/icon.png"
     t.window.borderless = true
     t.window.resizable  = false
 end
--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,17 @@
 local content = {}
+local text = 'Loading'
 
 function love.load()
-    content.thumb = love.graphics.newImage("Assets/thumb.png")
-    content.roboto = love.graphics.newFont("Assets/Fonts/Roboto/Roboto-Regular.ttf", 128)
+    content.icon    = love.image.newImageData("Assets/icon.png")
+    content.thumb   = love.graphics.newImage("Assets/thumb.png")
+    content.roboto  = love.graphics.newFont("Assets/Fonts/Roboto/Roboto-Regular.ttf", 50)
+    content.bar     = love.graphics.newImage("Assets/bar.png")
+
+    love.window.setIcon(content.icon)
 end
 
 function love.update(dt)
+    
 end
 
 function love.draw()
@@ -14,7 +20,8 @@ function love.draw()
     love.graphics.scale(0.5, 0.5)
     love.graphics.draw(content.thumb)
     love.graphics.pop()
-    love.graphics.setColor(0, 0, 0, 1)
+    love.graphics.draw(content.bar, 0, 440)
+    --love.graphics.setColor(0, 0, 0, 1)
     love.graphics.setFont(content.roboto)
-    love.graphics.print("Hello", 0, 0)
+    love.graphics.printf(text, 0, 475, 1920/2, "center")
 end
